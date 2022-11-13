35a7a5ab612b423715761ac5db1b1b6949869ea8
Divij Seth <divijseth@Divijs-MBP.hub>
2022-11-13 21:07:34 +1300

    Added in additional validation and sanitisation of fields

--- src/models/appointment.py
+++ src/models/appointment.py
@@ -14,6 +14,7 @@ class Appointment(db.Model):
     barber_id = db.Column(db.Integer, db.ForeignKey('barbers.id'), nullable = False)
     service_id = db.Column(db.Integer, db.ForeignKey('services.id'), nullable = False)
     
+    
     #relationships
     #appointment can have one user
     user = db.relationship('User', back_populates = 'appointment', uselist = False)
